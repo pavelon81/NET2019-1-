1adddffd582e397e87ccea8451fe246723583b1a
Ашаев Павел <pavelon81@gmail.com>
2019-03-06 10:30:03 +0300

    Задание 2. Ashaev_Pavel_Task03

---
+++ Ashaev_Pavel_Task02/Task4/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа формирования изображения из N треугольников.");
+            Console.WriteLine("Enter the Number of Triange");
+            int numTriange = Convert.ToInt32(Console.ReadLine());
+
+            string star = "*";
+            string space = " ";
+
+
+            if (numTriange >= 0)
+            {
+                for (int k = 1; k <= numTriange + 1; k++)
+                {
+                    int numLines = k;
+
+                    for (int i = 1; i < numLines; i++)
+                    {
+                        for (int j = i; j < numLines + numTriange - k; j++)
+                        {
+                            Console.Write(space);
+                        }
+                        for (int j = 1; j <= i; j++)
+                        {
+                            Console.Write(star);
+                        }
+                        for (int j = 1; j < i; j++)
+                        {
+                            Console.Write(star);
+                        }
+
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine(" Error! the Number will be > 0 ");
+            }
+            Console.ReadLine();
+
+
+
+
+        }
+    }
+}
---
+++ Ashaev_Pavel_Task02/Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа выводит на экран сумму всех чисел меньше 1000, кратных 3, или 5.");
+            var n = 1000;
+            var sum = 0;
+
+            Console.WriteLine(" Press 'Enter' ");
+            Console.ReadLine();
+
+            for (int i = 1; i <= n; i++) 
+            {
+                int result3 = i % 3;
+                int result5 = i % 5;
+
+                if ( (result3 == 0) | (result5 == 0) )
+                {
+                    sum = i + sum;
+                }
+            }
+
+            Console.WriteLine("Сумма всех чисел меньше 1000, кратных 3, или 5 = " + sum);
+            Console.ReadLine();
+        }
+    }
+}
